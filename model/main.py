97de0cc7676bebffc355c919bb8b2f86e7f886c3
ruserxd <zxcmomo0219@gmail.com>
2025-07-04 19:54:30 +0800

    refactor: spider problem
    - one time one project
    - Not only using SHA for checking dup because SHA in different project will have same
    -

--- model/main.py
+++ model/main.py
@@ -2,7 +2,7 @@ from transformers import T5ForConditionalGeneration, RobertaTokenizer
 
 # 翻譯與模型
 tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-base')
-model = T5ForConditionalGeneration.from_pretrained("./commit-model")
+model = T5ForConditionalGeneration.from_pretrained("commit-model")
 
 # 使用訓練好的模型
 text = ""
