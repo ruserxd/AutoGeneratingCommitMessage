3c74a9340d3800ba47af74a275c75b7f0abbe5f4
ruserxd <zxcmomo0219@gmail.com>
2025-07-15 13:43:38 +0800

    refactor: training.py and main.py
    - show token, Loss INFO

--- model/main.py
+++ model/main.py
@@ -1,34 +1,83 @@
 from transformers import T5ForConditionalGeneration, RobertaTokenizer
+import torch
 
+def test_model():
+    """測試模型生成功能"""    
+    model_path = "./commit-model"
+    tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-base')
+    model = T5ForConditionalGeneration.from_pretrained(model_path)
+    model.eval()
+    
+    # 測試用的 diff
+    test_diff = """diff --git a/src/main/java/com/thealgorithms/datastructures/heaps/HeapElement.java b/src/main/java/com/thealgorithms/datastructures/heaps/HeapElement.java\nindex 7c457a340645..20f33bd2d146 100644\n--- a/src/main/java/com/thealgorithms/datastructures/heaps/HeapElement.java\n+++ b/src/main/java/com/thealgorithms/datastructures/heaps/HeapElement.java\n@@ -1,14 +1,24 @@\n package com.thealgorithms.datastructures.heaps;\n \n /**\n- * Class for heap elements.<br>\n+ * Class representing an element in a heap.\n  *\n  * <p>\n- * A heap element contains two attributes: a key which will be used to build the\n- * tree (int or double, either primitive type or object) and any kind of\n- * IMMUTABLE object the user sees fit to carry any information he/she likes. Be\n- * aware that the use of a mutable object might jeopardize the integrity of this\n- * information.\n+ * A heap element contains two attributes: a key used for ordering in the heap\n+ * (which can be of type int or double, either as primitive types or as wrapper objects)\n+ * and an additional immutable object that can store any supplementary information the user desires.\n+ * Note that using mutable objects may compromise the integrity of this information.\n+ * </p>\n+ *\n+ * <p>\n+ * The key attribute is used to determine the order of elements in the heap,\n+ * while the additionalInfo attribute can carry user-defined data associated with the key.\n+ * </p>\n+ *\n+ * <p>\n+ * This class provides multiple constructors to accommodate various key types and includes\n+ * methods to retrieve the key and additional information.\n+ * </p>\n  *\n  * @author Nicolas Renard\n  */\n@@ -19,9 +29,10 @@ public class HeapElement {\n \n     // Constructors\n     /**\n-     * @param key : a number of primitive type 'double'\n-     * @param info : any kind of IMMUTABLE object. May be null, since the\n-     * purpose is only to carry additional information of use for the user\n+     * Creates a HeapElement with the specified key and additional information.\n+     *\n+     * @param key  the key of the element (primitive type double)\n+     * @param info any immutable object containing additional information, may be null\n      */\n     public HeapElement(double key, Object info) {\n         this.key = key;\n@@ -29,9 +40,10 @@ public HeapElement(double key, Object info) {\n     }\n \n     /**\n-     * @param key : a number of primitive type 'int'\n-     * @param info : any kind of IMMUTABLE object. May be null, since the\n-     * purpose is only to carry additional information of use for the user\n+     * Creates a HeapElement with the specified key and additional information.\n+     *\n+     * @param key  the key of the element (primitive type int)\n+     * @param info any immutable object containing additional information, may be null\n      */\n     public HeapElement(int key, Object info) {\n         this.key = key;\n@@ -39,9 +51,10 @@ public HeapElement(int key, Object info) {\n     }\n \n     /**\n-     * @param key : a number of object type 'Integer'\n-     * @param info : any kind of IMMUTABLE object. May be null, since the\n-     * purpose is only to carry additional information of use for the user\n+     * Creates a HeapElement with the specified key and additional information.\n+     *\n+     * @param key  the key of the element (object type Integer)\n+     * @param info any immutable object containing additional information, may be null\n      */\n     public HeapElement(Integer key, Object info) {\n         this.key = key;\n@@ -49,9 +62,10 @@ public HeapElement(Integer key, Object info) {\n     }\n \n     /**\n-     * @param key : a number of object type 'Double'\n-     * @param info : any kind of IMMUTABLE object. May be null, since the\n-     * purpose is only to carry additional information of use for the user\n+     * Creates a HeapElement with the specified key and additional information.\n+     *\n+     * @param key  the key of the element (object type Double)\n+     * @param info any immutable object containing additional information, may be null\n      */\n     public HeapElement(Double key, Object info) {\n         this.key = key;\n@@ -59,7 +73,9 @@ public HeapElement(Double key, Object info) {\n     }\n \n     /**\n-     * @param key : a number of primitive type 'double'\n+     * Creates a HeapElement with the specified key.\n+     *\n+     * @param key the key of the element (primitive type double)\n      */\n     public HeapElement(double key) {\n         this.key = key;\n@@ -67,7 +83,9 @@ public HeapElement(double key) {\n     }\n \n     /**\n-     * @param key : a number of primitive type 'int'\n+     * Creates a HeapElement with the specified key.\n+     *\n+     * @param key the key of the element (primitive type int)\n      */\n     public HeapElement(int key) {\n         this.key = key;\n@@ -75,7 +93,9 @@ public HeapElement(int key) {\n     }\n \n     /**\n-     * @param key : a number of object type 'Integer'\n+     * Creates a HeapElement with the specified key.\n+     *\n+     * @param key the key of the element (object type Integer)\n      */\n     public HeapElement(Integer key) {\n         this.key = key;\n@@ -83,7 +103,9 @@ public HeapElement(Integer key) {\n     }\n \n     /**\n-     * @param key : a number of object type 'Double'\n+     * Creates a HeapElement with the specified key.\n+     *\n+     * @param key the key of the element (object type Double)\n      */\n     public HeapElement(Double key) {\n         this.key = key;\n@@ -92,46 +114,57 @@ public HeapElement(Double key) {\n \n     // Getters\n     /**\n-     * @return the object containing the additional info provided by the user.\n+     * Returns the object containing the additional information provided by the user.\n+     *\n+     * @return the additional information\n      */\n     public Object getInfo() {\n         return additionalInfo;\n     }\n \n     /**\n-     * @return the key value of the element\n+     * Returns the key value of the element.\n+     *\n+     * @return the key of the element\n      */\n     public double getKey() {\n         return key;\n     }\n \n     // Overridden object methods\n+    /**\n+     * Returns a string representation of the heap element.\n+     *\n+     * @return a string describing the key and additional information\n+     */\n+    @Override\n     public String toString() {\n-        return \"Key: \" + key + \" - \" + additionalInfo.toString();\n+        return \"Key: \" + key + \" - \" + (additionalInfo != null ? additionalInfo.toString() : \"No additional info\");\n     }\n \n     /**\n-     * @param otherHeapElement\n-     * @return true if the keys on both elements are identical and the\n-     * additional info objects are identical.\n+     * Compares this heap element to another object for equality.\n+     *\n+     * @param o the object to compare with\n+     * @return true if the keys and additional information are identical, false otherwise\n      */\n     @Override\n     public boolean equals(Object o) {\n-        if (o != null) {\n-            if (!(o instanceof HeapElement)) {\n-                return false;\n-            }\n-            HeapElement otherHeapElement = (HeapElement) o;\n-            return ((this.key == otherHeapElement.key) && (this.additionalInfo.equals(otherHeapElement.additionalInfo)));\n+        if (o instanceof HeapElement otherHeapElement) {\n+            return this.key == otherHeapElement.key && (this.additionalInfo != null ? this.additionalInfo.equals(otherHeapElement.additionalInfo) : otherHeapElement.additionalInfo == null);\n         }\n         return false;\n     }\n \n+    /**\n+     * Returns a hash code value for the heap element.\n+     *\n+     * @return a hash code value for this heap element\n+     */\n     @Override\n     public int hashCode() {\n-        int result = 0;\n-        result = 31 * result + (int) key;\n-        result = 31 * result + (additionalInfo != null ? additionalInfo.hashCode() : 0);\n+        int result = 31 * (int) key;\n+        result += (additionalInfo != null) ? additionalInfo.hashCode() : 0;\n         return result;\n     }\n }\ndiff --git a/src/test/java/com/thealgorithms/datastructures/heaps/HeapElementTest.java b/src/test/java/com/thealgorithms/datastructures/heaps/HeapElementTest.java\nnew file mode 100644\nindex 000000000000..d04a9de8a94b\n--- /dev/null\n+++ b/src/test/java/com/thealgorithms/datastructures/heaps/HeapElementTest.java\n@@ -0,0 +1,55 @@\n+package com.thealgorithms.datastructures.heaps;\n+\n+import static org.junit.jupiter.api.Assertions.assertEquals;\n+import static org.junit.jupiter.api.Assertions.assertNotEquals;\n+import static org.junit.jupiter.api.Assertions.assertNull;\n+\n+import org.junit.jupiter.api.Test;\n+\n+class HeapElementTest {\n+\n+    @Test\n+    void testConstructorAndGetters() {\n+        HeapElement element = new HeapElement(5.0, \"Info\");\n+        assertEquals(5.0, element.getKey());\n+        assertEquals(\"Info\", element.getInfo());\n+    }\n+\n+    @Test\n+    void testConstructorWithNullInfo() {\n+        HeapElement element = new HeapElement(10);\n+        assertEquals(10, element.getKey());\n+        assertNull(element.getInfo());\n+    }\n+\n+    @Test\n+    void testToString() {\n+        HeapElement element = new HeapElement(7.5, \"TestInfo\");\n+        assertEquals(\"Key: 7.5 - TestInfo\", element.toString());\n+\n+        HeapElement elementWithoutInfo = new HeapElement(3);\n+        assertEquals(\"Key: 3.0 - No additional info\", elementWithoutInfo.toString());\n+    }\n+\n+    @Test\n+    void testEquals() {\n+        HeapElement element1 = new HeapElement(2.5, \"Data\");\n+        HeapElement element2 = new HeapElement(2.5, \"Data\");\n+        HeapElement element3 = new HeapElement(3.0, \"DifferentData\");\n+\n+        assertEquals(element1, element2); // Same key and info\n+        assertNotEquals(element1, element3); // Different key\n+        assertNotEquals(null, element1); // Check for null\n+        assertNotEquals(\"String\", element1); // Check for different type\n+    }\n+\n+    @Test\n+    void testHashCode() {\n+        HeapElement element1 = new HeapElement(4, \"HashMe\");\n+        HeapElement element2 = new HeapElement(4, \"HashMe\");\n+        HeapElement element3 = new HeapElement(4, \"DifferentHash\");\n+\n+        assertEquals(element1.hashCode(), element2.hashCode()); // Same key and info\n+        assertNotEquals(element1.hashCode(), element3.hashCode()); // Different info\n+    }\n+},
+"""
 
-model_path = "./commit-model"  
-tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-base')
-model = T5ForConditionalGeneration.from_pretrained(model_path)
+    print(f"📝 測試輸入:")
+    print(test_diff[:200] + "..." if len(test_diff) > 200 else test_diff)
+    
+    # Tokenization
+    inputs = tokenizer(
+        test_diff,
+        return_tensors="pt",
+        max_length=256,  # 縮短長度
+        truncation=True,
+        padding=True
+    )
+    
+    print(f"🔢 Input tokens: {inputs.input_ids.shape}")
+    print(f"🔢 First few tokens: {inputs.input_ids[0][:10]}")
+    
+    # 生成
+    with torch.no_grad():
+        output = model.generate(
+            inputs.input_ids,
+            max_length=25,              # 限制長度
+            no_repeat_ngram_size=3,     # 防止3-gram重複
+            repetition_penalty=1.3,     # 重複懲罰
+            do_sample=True,             # 啟用採樣
+            temperature=0.8,            # 降低確定性
+            top_p=0.9,                  # top-p採樣
+            pad_token_id=tokenizer.pad_token_id,
+        )
+    
+    print(f"🔢 Output tokens: {output.shape}")
+    print(f"🔢 Output token ids: {output[0]}")
+    
+    # 解碼
+    decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
+    
+    print(f"\n🎯 模型生成結果:")
+    print(f"'{decoded_output}'")
+    
+    return True
+def diagnose_tokenizer():
+    """診斷 tokenizer 問題"""
+    
+    print("\n🔍 診斷 tokenizer...")
+    
+    tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-base')
+    
+    # 檢查特殊 tokens
+    print(f"Vocab size: {tokenizer.vocab_size}")
+    print(f"PAD token: {tokenizer.pad_token} (id: {tokenizer.pad_token_id})")
+    print(f"EOS token: {tokenizer.eos_token} (id: {tokenizer.eos_token_id})")
+    print(f"UNK token: {tokenizer.unk_token} (id: {tokenizer.unk_token_id})")
+    
+    # 測試一些奇怪的 token ids
+    strange_ids = [36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+    print(f"\n檢查可疑的 token ids:")
+    for token_id in strange_ids:
+        try:
+            decoded = tokenizer.decode([token_id])
+            print(f"  ID {token_id}: '{decoded}'")
+        except:
+            print(f"  ID {token_id}: ERROR")
 
-text = """diff --git a/src/main/java/com/example/DataParser.java b/src/main/java/com/example/DataParser.java
-index 3456789..cdefghi 100644
---- a/src/main/java/com/example/DataParser.java
-+++ b/src/main/java/com/example/DataParser.java
-@@ -5,6 +5,10 @@ public class DataParser {
-     public int parseNumber(String value) {
--        return Integer.parseInt(value);
-+        try {
-+            return Integer.parseInt(value);
-+        } catch (NumberFormatException e) {
-+            return 0; // Default value on parsing failure
-+        }
-     }
- }"""
-
-input_ids = tokenizer(text, return_tensors="pt", max_length=512, truncation=True, padding=True).input_ids
-output = model.generate(input_ids, max_length=156, min_length=50 ,num_beams=5, no_repeat_ngram_size=3, early_stopping=True)
-
-
-decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
-try:
-    commit_message, why = decoded_output.split("\nWhy: ", 1)
-    print("Commit Message:", commit_message)
-    print("Why:", why)
-except ValueError:
-    print("Generated Output:", decoded_output)
-    print("Warning: Output format incorrect, unable to split into commit message and why")
+if __name__ == "__main__":
+    # 診斷 tokenizer
+    diagnose_tokenizer()
+    
+    print("\n" + "="*50)
+    
+    # 完整測試
+    success = test_model()
--- model/training.py
+++ model/training.py
@@ -1,7 +1,10 @@
 import json
 import torch
+import warnings
 from transformers import RobertaTokenizer, T5ForConditionalGeneration, Trainer, TrainingArguments, DataCollatorForSeq2Seq
 from datasets import Dataset
+
+warnings.filterwarnings("ignore")
 
 def main():
     # 檢查並設定 GPU
@@ -16,16 +19,22 @@ def main():
     # 初始化 tokenizer 和模型
     tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-base')
     model = T5ForConditionalGeneration.from_pretrained('Salesforce/codet5-base')
-
-    # 將模型移至 GPU
     model = model.to(device)
 
     # 指定資料路徑
-    json_file_path = './spider-data/training-data/spring-projects_spring-boot_20250713_235938.json'
+    json_file_path = './Tavernari-git-commit-message-dt/Tavernari-git-commit-message-dt_commit_message.json'
 
     # 讀取 JSON 檔案
     with open(json_file_path, 'r', encoding='utf-8') as f:
         training_data = json.load(f)
+
+    print(f"📊 原始資料: {len(training_data)} 筆")
+
+    # 顯示前幾筆資料
+    for i, item in enumerate(training_data[:3]):
+        print(f"第{i+1}筆:")
+        print(f"  Input: {item['input'][:100]}...")
+        print(f"  Output: {item['output']}")
 
     # 註解處理函數
     def handle_comments(diff):
@@ -33,38 +42,89 @@ def main():
         processed_lines = [line for line in lines if not line.strip().startswith('//')]
         return '\n'.join(processed_lines)
 
-    # 預處理函數
     def preprocess(examples):
-        # 處理 input 欄位
+        """完全修正的預處理函數"""
+        
+        # 處理 input
         processed_inputs = [handle_comments(item) for item in examples["input"]]
-        inputs = tokenizer(processed_inputs, max_length=512, truncation=True, padding=True)
         
-        # 處理 output 欄位 - 現在是字串而不是字典
-        outputs = tokenizer(examples["output"], max_length=128, truncation=True, padding=True)
-        inputs["labels"] = outputs["input_ids"]
+        # Tokenize inputs
+        model_inputs = tokenizer(
+            processed_inputs,
+            max_length=256,
+            truncation=True,
+            padding=True
+        )
         
-        return inputs
+        # Tokenize targets
+        labels = tokenizer(
+            examples["output"],
+            max_length=64,
+            truncation=True,
+            padding=True
+        )
+        
+        # 設定 labels
+        label_ids = []
+        for label_seq in labels["input_ids"]:
+            label_with_ignore = [
+                -100 if token == tokenizer.pad_token_id else token 
+                for token in label_seq
+            ]
+            label_ids.append(label_with_ignore)
+        
+        model_inputs["labels"] = label_ids
+        return model_inputs
 
     # 建立資料集
+    print(f"\n🔄 預處理資料...")
     dataset = Dataset.from_list(training_data).map(preprocess, batched=True)
+    
+    # 檢查第一筆處理後的資料
+    sample = dataset[0]
+    print(f"\n🔍 第一筆處理後的資料:")
+    print(f"Input IDs length: {len(sample['input_ids'])}")
+    print(f"Labels length: {len(sample['labels'])}")
+    print(f"Input text: {tokenizer.decode(sample['input_ids'], skip_special_tokens=True)[:100]}...")
+    
+    # 解碼 labels（排除 -100）
+    valid_labels = [t for t in sample['labels'] if t != -100]
+    print(f"Label text: {tokenizer.decode(valid_labels, skip_special_tokens=True)}")
 
     # 訓練設定
     training_args = TrainingArguments(
         output_dir="./commit-model",
-        num_train_epochs=3,
-        per_device_train_batch_size=4,
+        num_train_epochs=2,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=2,
         save_steps=50,
-        logging_steps=10,
+        logging_steps=5,
         save_total_limit=2,
         load_best_model_at_end=False,
         
-        # 安全使用多進程
-        dataloader_num_workers=2,
-        dataloader_pin_memory=True,
+        # 學習率設定
+        learning_rate=5e-5,
+        warmup_steps=10,
+        
+        # 資料載入設定
+        dataloader_num_workers=0,
+        dataloader_pin_memory=False,
         
         # GPU 優化
         fp16=torch.cuda.is_available(),
         no_cuda=False,
+        
+        # 其他設定
+        weight_decay=0.01,
+        logging_dir='./logs',
+        report_to=None,
+    )
+
+    # 資料整理器
+    data_collator = DataCollatorForSeq2Seq(
+        tokenizer=tokenizer,
+        model=model,
+        padding=True
     )
 
     # 建立訓練器
@@ -72,21 +132,27 @@ def main():
         model=model,
         args=training_args,
         train_dataset=dataset,
-        data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
+        data_collator=data_collator
     )
 
     # 開始訓練
-    print(f"開始訓練，使用資料集: {json_file_path}")
+    print(f"\n🚀 開始訓練...")
     print(f"資料集大小: {len(training_data)} 筆")
-    print(f"訓練設備: {device}")
+    print(f"批次大小: {training_args.per_device_train_batch_size}")
+    print(f"梯度累積: {training_args.gradient_accumulation_steps}")
+    print(f"有效批次大小: {training_args.per_device_train_batch_size * training_args.gradient_accumulation_steps}")
 
-    trainer.train()
+    # 訓練
+    train_result = trainer.train()
+    
+    print(f"\n📊 訓練結果:")
+    print(f"最終 Loss: {train_result.training_loss:.4f}")
 
     # 保存模型
     trainer.save_model("./commit-model")
     tokenizer.save_pretrained("./commit-model")
 
-    print("訓練完成！模型已保存到 ./commit-model")
+    print(f"\n✅ 訓練完成！模型已保存到 ./commit-model")
 
 if __name__ == '__main__':
     main()
